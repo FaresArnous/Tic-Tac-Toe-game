92e4fecfc7d15365dad588024608a2eae2cdaecb
fares Arnous <Faris.Arnous@hotmail.com>
2024-09-25 10:24:54 +0300

    added error message for invalid player name input

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 const openModel = document.getElementById("config-overlay");
 const backDrop = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
+const errorOutput = document.getElementById("config-errors");
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
--- script/config.js
+++ script/config.js
@@ -6,10 +6,18 @@ function openPlayerconfig() {
 function closePlayerconfig() {
   openModel.style.display = "none";
   backDrop.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorOutput.textContent = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayerName = formData.get("Playername");
+  const enteredPlayerName = formData.get("Playername").trim();
+
+  if (enteredPlayerName === "") {
+    event.target.firstElementChild.classList.add("error");
+    errorOutput.textContent = "Pleas enter a valid name!";
+    return;
+  }
 }
