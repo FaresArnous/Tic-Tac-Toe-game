6f1fb6030ed396f05236ebf0a6b229a40938c75e
fares Arnous <Faris.Arnous@hotmail.com>
2024-09-25 12:51:58 +0300

    keep tracking for the game board and wrot some functions to do so.

--- script/app.js
+++ script/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const player = [
   {
@@ -17,6 +24,8 @@ const formElement = document.querySelector("form");
 const errorOutput = document.getElementById("config-errors");
 const toastMsg = document.getElementById("toast-msg");
 const boardArea = document.getElementById("active-game");
+const gameBoardElements = document.querySelectorAll("#game-board li");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
@@ -32,3 +41,7 @@ backDrop.addEventListener("click", closePlayerconfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGame.addEventListener("click", startGame);
+
+for (const gameBoardElement of gameBoardElements) {
+  gameBoardElement.addEventListener("click", selectGameField);
+}
--- script/game.js
+++ script/game.js
@@ -8,6 +8,35 @@ function startGame() {
 
     return;
   }
+  activePlayerNameElement.textContent = player[activePlayer].name;
 
   boardArea.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = player[activePlayer].name;
+}
+
+function selectGameField(event) {
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedCRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedCRow][selectedColumn] > 0) {
+    alert("Please select an empty feild!!");
+    return;
+  }
+
+  selectedField.textContent = player[activePlayer].Symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedCRow][selectedColumn] = activePlayer + 1;
+
+  console.log(gameData);
+  switchPlayer();
 }
